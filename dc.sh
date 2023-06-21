ccdc8bfff2a0e8c736c3a33b4475b9553635ec46
chris <chris@tools.home.lan>
2023-06-21 17:51:36 +0000

    Initialize config

--- dc.sh
+++ dc.sh
@@ -1,42 +1,40 @@
 #!/bin/bash
 
 display_help() {
-    echo "Usage: $0 {build|start|down}" >&2
+    echo "Usage: $0 {build|start|down|env|credentials}" >&2
     echo
     exit 1
 }
 
-init() {
-  CURRENT_DIR=$(pwd)
-  DD_REPO="https://github.com/DefectDojo/django-DefectDojo"
-  DD_FOLDER="$(pwd)/$(echo $DD_REPO|rev|cut -d"/" -f 1|rev)"
-  # Show settings
-  echo -e "\033[0;32mSettings\033[0m"
-  echo "CURRENT_DIR: $CURRENT_DIR"
-  echo "DD_REPO: $DD_REPO"
-  echo "DD_FOLDER: $DD_FOLDER"
+# show environment variables
+show_env() {
+  echo -e "\033[0;32mEnvironment variables\033[0m"
+  grep -v "DD_DATABASE_PASSWORD" "$DD_FOLDER/docker/environments/$PROFILE.env"|grep -ve '^$'
+}
+
+# obtain admin credentials
+show_credentials() {
+  admin_password=$(docker-compose --log-level ERROR -f $DD_FOLDER/docker-compose.yml logs initializer\
+    |grep "Admin password:"|awk '{ print $5 }')
+  echo -e "\033[0;32madmin_user:\033[0m admin"
+  echo -e "\033[0;32madmin_password:\033[0m $admin_password"
 }
 
 # Build DefectDojo docker images (=~ 4min) : defectdojo-nginx, defectdojo-django
 build() {
   # Clone DefectDojo project
   if [ ! -d "$DD_FOLDER" ]; then
-    git clone $DD_REPO
+    git clone "$DD_REPO"
   fi
   cd "$DD_FOLDER" && source ./dc-build.sh && cd "$CURRENT_DIR" || exit
 }
 
 # Starting docker compose with the profile postgres-redis
 start() {
-  PROFILE="postgres-redis"
-  # show default environment variables
-  echo -e "\033[0;32mDefault environment variables\033[0m"
-  grep -v "DD_DATABASE_PASSWORD" "$DD_FOLDER/docker/environments/$PROFILE.env"|grep -ve '^$'
+  show_env
   # the initializer can take up to 3 minutes to run, use docker-compose logs -f initializer to track progress
-  cd "$DD_FOLDER" && source ./dc-up-d.sh $PROFILE && cd "$CURRENT_DIR" || exit
-  # obtain admin credentials
-  admin_password=$(grep "DD_DATABASE_PASSWORD" "$DD_FOLDER/docker/environments/$PROFILE.env"|cut -d"=" -f2-)
-  echo -e "\033[0;32madmin_password:\033[0m $admin_password"
+  cd "$DD_FOLDER" && source ./dc-up-d.sh "$PROFILE" && cd "$CURRENT_DIR" || exit
+  show_credentials
 }
 
 # Remove DefectDojo containers/volumes
@@ -46,19 +44,37 @@ down() {
   docker volume rm $(docker volume ls --filter name=django-defectdojo) &> /dev/null
 }
 
+CURRENT_DIR=$(pwd)
+DD_REPO="https://github.com/DefectDojo/django-DefectDojo"
+DD_FOLDER="$(pwd)/$(echo $DD_REPO|rev|cut -d"/" -f 1|rev)"
+PROFILE="postgres-redis"
+DEBUG=0
+
+# Show settings
+if [ $DEBUG -eq 1 ]; then
+  echo -e "\033[0;32mCurrent settings\033[0m"
+  echo "CURRENT_DIR: $CURRENT_DIR"
+  echo "DD_REPO: $DD_REPO"
+  echo "DD_FOLDER: $DD_FOLDER"
+  echo "PROFILE: $PROFILE"
+fi
+
 case "$1" in
   build)
-    init
     build
     ;;
   start)
-    init
     build
     start
     ;;
   down)
-    init
     down
+    ;;
+  env)
+    show_env
+    ;;
+  credentials)
+    show_credentials
     ;;
   *)
      display_help
