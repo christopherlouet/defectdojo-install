05c568a4d20239655a794f0deeeea83dbaf9af13
Christopher LOUËT <christopher.louet@pm.me>
2023-07-16 17:05:19 +0000

    Improve application starting, adding release update and credentials

--- ddadmin.sh
+++ ddadmin.sh
@@ -34,32 +34,6 @@ _show_message() {
   echo -e "$msg_start$msg$msg_end"
 }
 
-# Check the current profile
-_profile_check() {
-  _show_message "Check current profile..."
-#  todo, to check the profile.env file...
-}
-
-# Edit the current profile
-_profile_edit() {
-  _show_message "Edit profile..."
-
-}
-
-# Create a token api
-_auth_create_token() {
-  docker-compose --log-level ERROR -f "$DD_FOLDER/docker-compose.yml" exec celerybeat bash -c "/app/manage.py changepassword"
-  #  docker-compose --log-level ERROR -f "$DD_FOLDER/docker-compose.yml" exec celerybeat bash -c "/app/manage.py drf_create_token"
-#  docker-compose --log-level ERROR -f "$DD_FOLDER/docker-compose.yml" exec celerybeat bash -c "/app/manage.py dumpdata --skip-checks auth.user"
-}
-
-# Get the current token api
-_auth_get_token() {
-  command="/app/manage.py dumpdata --skip-checks authtoken.token"
-  token=$(docker-compose --log-level ERROR -f "$DD_FOLDER/docker-compose.yml" exec celerybeat bash -c "$command"|jq -r '.[0].pk')
-  echo "TOKEN: $token"
-}
-
 # Create a profile with the default file
 _profile_create() {
   if [ -d "$PROFILE_DEFAULT_FOLDER" ]; then
@@ -90,7 +64,7 @@ _profile_create() {
     response=0
     while [ $response -lt 1 ] || [ $response -gt $choice ]; do
       _show_message "$message" -1
-      confirm_message="\e[32mChoice a default profile (default $choice_default) [1-$choice] \e[0m"
+      confirm_message="Choice a default profile (default $choice_default) [1-$choice] "
       echo -ne "\e[32m$confirm_message\e[0m"; read -r -e -p "${confirm_message//?/$'\a'}" response
       if [ -z "$response" ]; then
         response=$choice_default
@@ -112,8 +86,20 @@ _profile_create() {
     # Copy default profile in the project folder
     _show_message "create default profile..."
     cp "$profile_default_file_path_target" "$PROFILE_FILE"
-    # Tag the profile in the file
+    # Change the default database password
+    db_password=""
+    while [ -z "$db_password" ]; do
+      confirm_message="Choice a database password : "
+          echo -ne "\e[32m$confirm_message\e[0m"; read -r -e -p "${confirm_message//?/$'\a'}" db_password
+    done
+    db_database_url="postgresql\:\/\/defectdojo\:$db_password\@postgres\:5432\/defectdojo"
+    sed -i -e "/DD_DATABASE_PASSWORD=/s/=.*/=$db_password/" "$PROFILE_FILE"
+    sed -i -e "/DD_DATABASE_URL=/s/=.*/=$db_database_url/" "$PROFILE_FILE"
+    # Tag the profile in the profile file
     echo "DD_PROFILE=$profile_default_file_target">>"$PROFILE_FILE"
+    # Tag the release in the profile file
+    release=$(cd "$DD_FOLDER" && git describe)
+    echo "DD_RELEASE=$release">>"$PROFILE_FILE"
   # Folder not exist...
   else
     _show_message "Folder $PROFILE_DEFAULT_FOLDER not exist!" 1
@@ -121,16 +107,65 @@ _profile_create() {
   fi
 }
 
+# Initialize the profile
+_profile_init() {
+  # Check if the profile file exist
+  if [ ! -f "$PROFILE_FILE" ]; then
+      message="Currently, no profile is configured."
+      exit 1
+  fi
+  # Set variables for current setting
+  PROFILE=$(grep "DD_PROFILE" "$PROFILE_FILE"|cut -d'=' -f2)
+  RELEASE=$(grep "DD_RELEASE" "$PROFILE_FILE"|cut -d'=' -f2)
+}
+
+# Clone the project
+_project_clone() {
+  if [ -n "$1" ]; then
+    release=$1
+  else
+    message_clone_latest=$(_show_confirm_message "Clone the latest version of DefectDojo ($DD_RELEASE_LATEST)? [Y/n] " "y")
+    if [ -z "$message_clone_latest" ]; then
+      release=""
+      while [ -z "$release" ]; do
+        read -r -p "Enter release version: " response_release
+        release=$(curl  -s "$DD_REPO_API/tags"|jq -r ".[]|select( .name == \"$response_release\" ).name")
+        if [ -z "$release" ]; then
+          _show_message "$response_release is not a valid version!" 1
+        fi
+      done
+    else
+      release=$DD_RELEASE_LATEST
+    fi
+  fi
+  # Clone the DefectDojo project with the release selected
+  _show_message "Clone the DefectDojo project ($release)"
+  rm -rf "$DD_FOLDER" && git clone --depth 1 --branch "$release" "$DD_REPO"
+}
+
+# Update the release
+_check_release_update() {
+  if [ ! "$RELEASE" = "$DD_RELEASE_LATEST" ]; then
+    message="A new version of the DefectDojo project has been released: ${DD_RELEASE_LATEST}. Current version is ${RELEASE}. Do you want to update? [y/N] "
+    update_project=$(_show_confirm_message "$message")
+    if [ -n "$update_project" ]; then
+      _project_clone "$DD_RELEASE_LATEST"
+      # Update the version
+      sed -i -e "/DD_RELEASE=/s/=.*/=$DD_RELEASE_LATEST/" "$PROFILE_FILE"
+    fi
+  fi
+}
+
 # Check app status
 _app_status() {
-  docker-compose --log-level ERROR -f "$DD_FOLDER/docker-compose.yml" ps -q celerybeat&>/dev/null
+  docker-compose --log-level ERROR -f "$DD_FOLDER/docker-compose.yml" ps -q "$DD_CONTAINER_ADM" &>/dev/null
   # shellcheck disable=SC2181
   if [ ! $? -eq 0 ]; then
       echo "down"
   else
     # shellcheck disable=SC2046
     app_status=$(docker ps -q --no-trunc|\
-            grep $(docker-compose --log-level ERROR -f "$DD_FOLDER/docker-compose.yml" ps -q celerybeat))
+            grep $(docker-compose --log-level ERROR -f "$DD_FOLDER/docker-compose.yml" ps -q "$DD_CONTAINER_ADM"))
     if [ -z "$app_status" ]; then
       echo "stop"
     else
@@ -168,14 +203,34 @@ _waiting_start() {
   printf " done (%ss)\033[0m\n" $runtime
 }
 
+# Get the current token API
+_auth_get_token() {
+  command="/app/manage.py dumpdata --skip-checks authtoken.token"
+  token=$(docker-compose --log-level ERROR -f "$DD_FOLDER/docker-compose.yml" exec celerybeat bash -c "$command"|jq -r '.[0].pk')
+  echo -e "\033[0;32mTOKEN:\033[0m $token"
+}
+
+# Update the token API
+_update_token() {
+  command="/app/manage.py drf_create_token admin 2> /dev/null"
+  docker-compose --log-level ERROR -f "$DD_FOLDER/docker-compose.yml" exec "$DD_CONTAINER_ADM" bash -c "$command"
+}
+
+# Update the admin password
+_update_admin_password() {
+  docker-compose --log-level ERROR -f "$DD_FOLDER/docker-compose.yml" exec "$DD_CONTAINER_ADM" bash -c "/app/manage.py changepassword admin"
+}
+
 # Display help :)
 display_help() {
   if [ "$1" = "show" ]; then
-     _show_message "Usage: $0 show {status|release|env|credentials|token|logs}" >&2
+     _show_message "Usage: $0 show {status|release|env|token|logs}" >&2
   elif [ "$1" = "stop" ]; then
     _show_message "Usage: $0 stop [--remove]" >&2
+  elif [ "$1" = "update" ]; then
+    _show_message "Usage: $0 update {token|credential}" >&2
   else
-    _show_message "Usage: $0 {start|stop|init|show}" >&2
+    _show_message "Usage: $0 {start|stop|show|update}" >&2
   fi
   echo
   exit 1
@@ -183,116 +238,67 @@ display_help() {
 
 # Initialize the project
 project_init() {
-  new_profile=0
-  if [ ! -f "$PROFILE_FILE" ] || [ ! -d "$DD_FOLDER" ]; then
-    # Clone DefectDojo project if the folder not exist
-    if [ ! -d "$DD_FOLDER" ]; then
-      _show_message "Clone the DefectDojo project"
-      release=$(curl -s "$DD_REPO_API/releases/latest"|jq -r .tag_name)
-      message_clone_latest=$(_show_confirm_message "Clone the latest version of DefectDojo ($release)? [Y/n] " "y")
-      if [ -z "$message_clone_latest" ]; then
-        release=""
-        while [ -z "$release" ]; do
-          read -r -p "Enter release version: " response_release
-          release=$(curl  -s "$DD_REPO_API/tags"|jq -r ".[]|select( .name == \"$response_release\" ).name")
-          if [ -z "$release" ]; then
-            _show_message "$response_release is not a valid version!" 1
-          fi
-        done
-      fi
-      git clone --depth 1 --branch "$release" "$DD_REPO"
-    fi
+  # Clone DefectDojo project if the folder not exist
+  if [ ! -d "$DD_FOLDER" ]; then
+    _project_clone
+  fi
+  # Initialize the profile if the file not exist
+  if [ ! -f "$PROFILE_FILE" ]; then
     message="Currently, no profile is configured. Do you want to use choose one? [Y/n] "
     choice_profile=$(_show_confirm_message "$message" "y")
     if [ -n "$choice_profile" ]; then
-      _profile_create && new_profile=1
+      _profile_create
+      _profile_init
+      _show_message "The project is now configured with the $PROFILE profile and the release $RELEASE"
     else
       _show_message "Please configure a profile before starting the application!" 1
       exit 1
     fi
-  fi
-  PROFILE=$(grep "DD_PROFILE" "$PROFILE_FILE"|cut -d'=' -f2)
-  release=$(cd "$DD_FOLDER" && git describe)
-  if [ $new_profile -eq 1 ]; then
-    _show_message "The project is now configured with the $PROFILE profile and the release $release"
-  fi
-}
-
-show() {
-  # show application status
-  if [ "$1" = "status" ]; then
-    app_status=$(_app_status)
-    if [ "$app_status" = "up" ]; then
-      _show_message "Application is started."
-    elif [ "$app_status" = "stop" ]; then
-      _show_message "Application is stopped."
-    else
-      _show_message "Application was never launched..."
-    fi
-  fi
-  # show current release
-  if [ "$1" = "release" ]; then
-    if [ ! -d "$DD_FOLDER" ]; then
-      _show_message "Project DefectDojo not exist, have you already started the application?" 1
-    else
-      release=$(cd "$DD_FOLDER" && git describe)
-      _show_message "Current release: $release"
-    fi
-  fi
-  # show environment variables
-  if [ "$1" = "env" ]; then
-    project_init
-    _show_message "Environment variables"
-    grep -v "DD_DATABASE_PASSWORD" "profile.env"|grep -ve '^$'
-  fi
-  # show credentials
-  if [ "$1" = "credentials" ]; then
-    _show_message "Obtaining credentials..."
-    admin_password=$(docker-compose --log-level ERROR -f "$DD_FOLDER/docker-compose.yml" logs initializer\
-      |grep "Admin password:"|grep -v "Initialization"|awk '{ print $5 }')
-    if [ -z "$admin_password" ]; then
-      _show_message "No credentials... Have you started the application yet? (./ddadmin.sh start)"
-    else
-      echo -e "\033[0;32madmin_user:\033[0m admin"
-      echo -e "\033[0;32madmin_password:\033[0m $admin_password"
-    fi
-  fi
-#  # show api token...
-#
-  # Shown container logs
-  if [ "$1" = "logs" ]; then
-    app_status=$(_app_status)
-    if [ ! "$app_status" = "up" ]; then
-      _show_message "Application is not started!"
-    else
-      docker-compose --log-level ERROR -f "$DD_FOLDER/docker-compose.yml" logs -f celerybeat
-    fi
+  else
+    _profile_init
   fi
 }
 
 # Starting docker compose with the profile postgres-redis
 start() {
-  project_init
   app_status=$(_app_status)
+  first_start=0
   if [ "$app_status" = "up" ]; then
     _show_message "Application is already started!"
-  else
-    if [ "$app_status" = "down" ]; then
-      # Build DefectDojo docker images (=~ 4min) : defectdojo-nginx, defectdojo-django
-      _show_message "Image building..."
-      cd "$DD_FOLDER" && source ./dc-build.sh >&/dev/null && cd "$CURRENT_DIR" || exit
-    fi
-    _show_message "Starting application..."
-    docker-compose -f "$DD_FOLDER/docker-compose.yml" --profile "$PROFILE" --env-file "profile.env" up --no-deps -d
-    _waiting_start
-    _show_message "Done! To display the credentials, issue the command: ./ddadmin.sh show credentials"
+    exit 0
   fi
+  if [ "$app_status" = "down" ]; then
+    first_start=1
+  fi
+  project_init
+  _check_release_update
+  # Build DefectDojo docker images (=~ 4min) : defectdojo-nginx, defectdojo-django
+  if [ $first_start -eq 1 ]; then
+    _show_message "Image building..."
+    docker-compose -f "$DD_FOLDER/docker-compose.yml" --profile "$PROFILE" --env-file "profile.env" build
+  fi
+  _show_message "Starting application..."
+  docker-compose -f "$DD_FOLDER/docker-compose.yml" --profile "$PROFILE" --env-file "profile.env" up --no-deps -d
+  _waiting_start
+  # Creating a token on first start, and show credentials
+  if [ $first_start -eq 1 ]; then
+    _update_token
+    admin_password=$(docker-compose --log-level ERROR -f "$DD_FOLDER/docker-compose.yml" logs initializer\
+        |grep "Admin password:"|grep -v "Initialization"|awk '{ print $5 }')
+    local_ip=$(ifconfig|grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*'|grep -Eo '([0-9]*\.){3}[0-9]*'|grep -v '172.*'|grep -v '127.*')
+    _show_message "Done! You can access to the web interface at this address: http://$local_ip:8080, with this credentials (save them):"
+    echo -e "\033[0;32madmin_user:\033[0m admin"
+    echo -e "\033[0;32madmin_password:\033[0m $admin_password"
+  else
+    _show_message "Done!"
+  fi
+  exit 0
 }
 
 # Stop/remove DefectDojo containers
 stop() {
   option=$1
-  project_init
+  _profile_init
   # Remove DefectDojo containers/volumes
   if [[ -n $option ]] && [ "$option" = "--remove" ]; then
     are_you_sure=$(_show_confirm_message "Are you sure? [y/N] ")
@@ -301,6 +307,7 @@ stop() {
       docker-compose -f "$DD_FOLDER/docker-compose.yml" --profile "$PROFILE" --env-file "profile.env" down
       # shellcheck disable=SC2046
       docker volume rm $(docker volume ls --filter name=django-defectdojo) &>/dev/null
+      rm -f profile.env
       _show_message "Done!"
     fi
   fi
@@ -315,13 +322,73 @@ stop() {
       _show_message "Done!"
     fi
   fi
+  exit 0
+}
+
+show() {
+  # show application status
+  if [ "$1" = "status" ]; then
+    app_status=$(_app_status)
+    if [ "$app_status" = "up" ]; then
+      _show_message "Application is started."
+    elif [ "$app_status" = "stop" ]; then
+      _show_message "Application is stopped."
+    else
+      _show_message "Application was never launched..."
+    fi
+  fi
+  # show release
+  if [ "$1" = "release" ]; then
+    if [ ! -d "$DD_FOLDER" ]; then
+      _show_message "Project DefectDojo not exist, have you already started the application?" 1
+    else
+      _profile_init
+    fi
+    _show_message "Current release: $RELEASE"
+    _show_message "Latest release: $DD_RELEASE_LATEST"
+  fi
+  # show environment variables
+  if [ "$1" = "env" ]; then
+    project_init
+    _show_message "Environment variables"
+    grep -v "DD_DATABASE_PASSWORD" "profile.env"|grep -v 'DD_TEST_DATABASE_URL'|grep -ve '^$'
+  fi
+  # show api token
+  if [ "$1" = "token" ]; then
+    command="/app/manage.py dumpdata --skip-checks authtoken.token 2> /dev/null"
+    token=$(docker-compose --log-level ERROR -f "$DD_FOLDER/docker-compose.yml" exec "$DD_CONTAINER_ADM" bash -c "$command"|jq -r '.[0].pk')
+    echo "TOKEN: $token"
+  fi
+  # Shown container logs
+  if [ "$1" = "logs" ]; then
+    app_status=$(_app_status)
+    if [ ! "$app_status" = "up" ]; then
+      _show_message "Application is not started!"
+    else
+      docker-compose --log-level ERROR -f "$DD_FOLDER/docker-compose.yml" logs -f "$DD_CONTAINER_ADM"
+    fi
+  fi
+  exit 0
+}
+
+update() {
+  if [ "$1" = "token" ]; then
+    _update_token
+  #  docker-compose --log-level ERROR -f "$DD_FOLDER/docker-compose.yml" exec "$DD_CONTAINER_ADM" bash -c "/app/manage.py dumpdata --skip-checks auth.user"
+  fi
+  if [ "$1" = "credential" ]; then
+    _update_admin_password
+  fi
+  exit 0
 }
 
 # Script variables
-CURRENT_DIR=$(pwd)
 DD_REPO="https://github.com/DefectDojo/django-DefectDojo"
 DD_REPO_API="https://api.github.com/repos/DefectDojo/django-DefectDojo"
 DD_FOLDER="$(pwd)/$(echo $DD_REPO|rev|cut -d"/" -f1|rev)"
+DD_RELEASE_LATEST=$(curl -s "$DD_REPO_API/releases/latest"|jq -r .tag_name)
+DD_CONTAINER_ADM=celerybeat
+
 PROFILE_FILE="profile.env"
 PROFILE_DEFAULT_FOLDER="$DD_FOLDER/docker/environments"
 PROFILE_DEFAULT="postgres-redis"
@@ -349,13 +416,18 @@ else
       if [ ! $options -eq 2 ]; then
         display_help show
       else
-        if [[ ! $2 =~ status|release|env|credentials|token|logs ]]; then display_help show; fi
+        if [[ ! $2 =~ status|release|env|token|logs ]]; then display_help show; fi
         show "$2"
       fi
       ;;
-    init)
-      project_init
-      ;;
+    update)
+        if [ ! $options -eq 2 ]; then
+          display_help update
+        else
+          if [[ ! $2 =~ token|credential ]]; then display_help update; fi
+          update "$2"
+        fi
+        ;;
     *)
       display_help
       ;;
